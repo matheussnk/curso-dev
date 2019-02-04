5d82338dbf168395f40ce0ebb97c07081e09ad09
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-02-04 15:55:43 -0200

    Update tpl_strings.js

--- fundamentos-js/tpl_strings.js
+++ fundamentos-js/tpl_strings.js
@@ -1,8 +1,13 @@
+// Template string aplicado no ECMA 6
+
 const nome = 'Matheus'
 const concatenacao = 'Olá ' + nome + ' !'
 const template = 
     `
-Olá ${nome} !`
- console.log(concatenacao)
- console.log(template)
+Olá ${nome} !`// O template aceita a formatação através de ENTER e tabulação e era afetar em sua exibição.
+ console.log(concatenacao,template)
+ // expressões 
+ console.log(`1 +1 = ${1 + 1}`)
+ const up = texto => texto.toUpperCase() 
 
+ console.log(`Ei... ${up('cuidado')}!`)// Tratamento com uppercase como alerta para usuarios
