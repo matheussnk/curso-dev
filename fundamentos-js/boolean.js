71da1c64468ed0ff09ea7b1948fb0a24a3846e79
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-02-04 18:29:23 -0200

    Add files via upload

---
+++ fundamentos-js/boolean.js
@@ -0,0 +1,34 @@
+// Operações que retornão TRUE ou FALSE
+const tpl =  ``
+var r = "Resutado do Ternario "
+
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo) // Negado duas vezes eke ira retornar o valor normal
+
+isAtivo = 1
+console.log("Negando uma unica vezes ele retornará o valor oposto  "+!isAtivo) // Negando uma unica vezes ele retornará o valor oposto
+console.log(tpl)
+console.log('Os ternarios com resultados verdadeiros ...')
+console.log(r+"!!3 "+!!3)
+console.log(r+"!!1"+!!-1)
+console.log(r+"vazio "+!!" ")
+console.log(r+"!![] "+!![])
+console.log(r+"!!{} "+!!{})
+console.log(r+"ternario do Infinity "+!!Infinity)
+console.log(r+"ternario do isAtivo Verdadeiro "+!!(isAtivo = true))
+console.log(tpl)
+console.log('Ternarios com resultados falsos ...')
+console.log(r+"!3 "+!3)
+console.log(r+"!-1 "+!-1)
+console.log(r+"do ternario vazio "+!" ")
+console.log(r+"do ternario array vazio "+![])
+console.log(r+"ternario dos objetos vazio "+!{})
+console.log(r+"ternario do Infinity "+!Infinity)
+console.log(r+"isAtivo negado  "+!(isAtivo = true))
+console.log(tpl)
