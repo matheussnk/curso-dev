9fa0b0c5c9434c74d16702a3d896e0d14e5127ca
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-02-22 13:50:38 -0300

    Update var.js

--- fundamentos-js/var.js
+++ fundamentos-js/var.js
@@ -4,8 +4,15 @@
                {
                     var sera = 'Será ???'
                     console.log(sera)
-            }
-        } 
-    }
+               }
+          }
+     }
 }
 console.log(sera)
+
+function teste() {
+     var local = 123
+     console.log(local)
+}
+teste()
+// console.log(local)
