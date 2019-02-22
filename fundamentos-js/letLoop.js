ba02f9f9fc8bc6cf7be70d9ac580f932397aaf94
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-02-22 15:59:57 -0300

    Create letLoop.js

---
+++ fundamentos-js/letLoop.js
@@ -0,0 +1,3 @@
+for (var i = 0; i < 10; i++) { // para cada repetição ele incrementará um a mais a cada vez que for rodada
+    console.log('i =', i)// valor de i será baseado no anterior ao que está definido como parametro para a finalização do comando 
+}
