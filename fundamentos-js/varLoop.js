51d6b97a2b2bcfaa201a3ca0178c42eb1bb4b4aa
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-02-22 15:49:06 -0300

    Create varLoop.js

---
+++ fundamentos-js/varLoop.js
@@ -0,0 +1,4 @@
+for (var i = 0; i < 10; i++){ // para cada repetição ele incrementará um a mais a cada vez que for rodada
+    console.log(i)
+}
+console.log('i =',i)// valor de i apresentado nesse console.log será o valor que o fez sair do laço
