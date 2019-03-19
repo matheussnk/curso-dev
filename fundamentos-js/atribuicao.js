a947e0af72923e9b63e4df71b4fc784c6ff0247e
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-03-19 11:38:15 -0300

    commit atribuicao.js

---
+++ fundamentos-js/atribuicao.js
@@ -0,0 +1,8 @@
+const a = 7
+let b = 3
+b += a // b = b + a = 10 
+console.log('Resultado = '+ b)
+b -= 4 // b = b - a = 6
+console.log('Resultado = '+ b)
+b *= 2 // b = b * 2
+console.log('Resultado = '+ b)
