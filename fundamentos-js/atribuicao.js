5e385da2fe75da686fe3a852c8c23d1f57beb0bc
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-03-19 11:44:37 -0300

    acréscimo de atribuições

--- fundamentos-js/atribuicao.js
+++ fundamentos-js/atribuicao.js
@@ -6,3 +6,7 @@ b -= 4 // b = b - a = 6
 console.log('Resultado = '+ b)
 b *= 2 // b = b * 2
 console.log('Resultado = '+ b)
+b /= 2 // b = b * 2
+console.log('Resultado = '+ b)
+b %= 2 // b = b * 2
+console.log('Resultado = '+ b)// Por ser par ele retornará o valor 0 caso fosse impar seria 1 
