dea549cb1dfe58f23f498ed90ff9e7c1c94b3a36
Matheus Mendonça Lima <lima.mm2011@gmail.com>
2019-04-15 15:00:08 -0300

    relacionais.js

---
+++ fundamentos-js/relacionais.js
@@ -0,0 +1,16 @@
+console.log('01)','1' == 1) //igualdade entre valores
+console.log('02)','1' === 1) //extreitamente igualdade 
+console.log('03)','3' != 3) //diferente sem levar em conta tipo  
+console.log('04)','3' !== 3) //extreitamente diferente sem levar em conta tipo  
+console.log('05)',3 < 2)//3 menor que 2 
+console.log('06)',3 > 2)//3 maior que 2 ?
+console.log('07)',3 <= 2)
+console.log('08)',3 >= 2)
+
+const d1 = new Date(0) // 1/1/1970
+const d2 = new Date(0)
+console.log('09)', d1 === d2)
+console.log('10)', d1 == d2)
+console.log('11)', d1.getTime() == d2.getTime())
+console.log('11)', undefined == null)
+console.log('11)', undefined === null)
