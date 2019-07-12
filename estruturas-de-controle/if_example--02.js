ac15423fcb67490a2f2d140fdfaae1092e6b16c7
matheussnk <lima.mm2011@gmail.com>
2019-07-12 11:58:19 -0300

    Add new file

---
+++ estruturas-de-controle/if_example--02.js
@@ -0,0 +1,19 @@
+// tbulação correta quando nao se usar chaves '{}"  é recomendado o uso das chaves para ter um cuidado maior com o que será executado
+function teste1(num) {
+    if (num > 7)
+        console.log(num)
+    console.log('Final')
+
+}
+teste1(8)
+teste1(6)
+
+
+function teste2(num) {
+    if (num > 7)
+        console.log(num)
+    console.log('Final')
+
+}
+teste2(8)
+teste2(6)
