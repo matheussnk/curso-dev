83fdead7efc099d5f191de1b7bdf342edf7e3160
matheussnk <lima.mm2011@gmail.com>
2020-02-13 17:41:55 -0300

    for example 1

---
+++ estruturas-de-controle/for.js
@@ -0,0 +1,15 @@
+let count = 1
+while (count <= 10) {
+    console.log(`count = ${count}`)
+    count++
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for (let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
